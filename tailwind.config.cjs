46c6586085ad4a157037399e5bd90c4a12aeb243
ztk37 <ztk37.contact@gmail.com>
2023-08-12 19:36:23 +0200

    chore: Reverse conversion

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,9 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx}"],
   darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
-} satisfies Config;
+};
